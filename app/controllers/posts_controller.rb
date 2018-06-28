6ee375176af5845ffb99392cb7daa01181e9980f
Riley Radoll <rradoll@gmail.com>
2018-06-27 23:27:37 -0400

    adds missing '!' to update call

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     @posts.each_with_index do |post, index|
-      post.update(title: 'SPAM' ) if index % 5 == 0
+      post.update!(title: 'SPAM' ) if index % 5 == 0
     end
   end
 
