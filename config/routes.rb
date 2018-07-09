0a675d6f2495e49a352ffe9a199e27ef4573db6f
Riley Radoll <rradoll@gmail.com>
2018-07-09 11:45:14 -0400

    Checkpoint 30 signing in: completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
   end
-
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
 
