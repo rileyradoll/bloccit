f9ad9a2fa908d36326a8f5aba3b18ff5bf8a068b
Riley Radoll <rradoll@gmail.com>
2018-07-09 00:12:34 -0400

    Assignment 29 completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
   end
-
   resources :users, only: [:new, :create]
+  post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
 
