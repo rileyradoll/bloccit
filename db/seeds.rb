379b3488c763859a663c5a51fa3f0debf601c410
Riley Radoll <rradoll@gmail.com>
2018-07-10 21:14:08 -0400

    Checkpoint 34 posts and users: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
 require 'random_data'
 
+# Create Users
+5.times do
+  User.create!(
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 # Create topics
 15.times do
   Topic.create!(
@@ -7,11 +17,14 @@ require 'random_data'
     description: RandomData.random_paragraph
   )
 end
+
 topics = Topic.all
 
 # Create Posts
 50.times do
   Post.create!(
+    user: users.sample,
+    topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
@@ -26,7 +39,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'youremail.com',
+  password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
