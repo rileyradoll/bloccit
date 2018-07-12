fa0d44cb317b5cdd055f5a85a6dcad6ce0606afd
Riley Radoll <rradoll@gmail.com>
2018-07-12 17:53:09 -0400

    Checkpoint 36 comments: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,8 +56,9 @@ end
 posts = Post.all
 
 # Create Comments
-50.times do
+100.times do
   Comment.create!(
+    user: users.sample,
     post: posts.sample,
     body: RandomData.random_paragraph
   )
