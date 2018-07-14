e5b74adb2951e721d809b95b0b35ad2eba551f2c
Riley Radoll <rradoll@gmail.com>
2018-07-13 22:51:59 -0400

    Assignment 37 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  after_create :create_vote
 
   default_scope { order(rank: :desc) }
-  scope :ordered_by_title, -> { order(title: :desc) }
-  scope :ordered_by_reverse_created_at, -> { order(created_at: :asc) }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
@@ -29,5 +28,11 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -31,8 +31,8 @@ RSpec.describe Post, type: :model do
 
   describe "voting" do
     before do
-      3.times { post.votes.create!(value: 1) }
-      2.times { post.votes.create!(value: -1) }
+      # 3.times { post.votes.create!(value: 1) }
+      # 2.times { post.votes.create!(value: -1) }
       @up_votes = post.votes.where(value: 1).count
       @down_votes = post.votes.where(value: -1).count
     end
@@ -52,6 +52,22 @@ RSpec.describe Post, type: :model do
     describe "#points" do
       it "returns the sum of all down and up votes" do
         expect( post.points ).to eq(@up_votes - @down_votes)
+      end
+    end
+
+    describe "#create_vote" do
+      it "sets the post up_votes to 1" do
+        expect(post.up_votes).to eq(1)
+      end
+
+      it "calls #create_vote when a post is created" do
+        post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_sentence, user: user)
+        expect(post).to receive(:create_vote)
+        post.save
+      end
+
+      it "associates the vote with the owner of the post" do
+        expect(post.votes.first.user).to eq(post.user)
       end
     end
 
