5d5a3061d817da8b2813ab8f4e91635d9775ee51
Riley Radoll <rradoll@gmail.com>
2018-07-18 01:46:39 -0400

    fixes user show view regarding displaying favorites.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -14,4 +14,13 @@ module UsersHelper
       "#{current_user.name} has not submitted any comments yet."
     end
   end
+
+  def user_favorites_if_any(favorites)
+    if favorites.any?
+      render @user.favorited_posts
+    else
+      "#{current_user.name} has not favorited any posts yet."
+    end
+  end
+
 end
